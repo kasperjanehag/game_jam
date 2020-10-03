9ab1e2a6337708317991dcd8ec3935b161fbd7ec
Kasper Janehag <kasper.janehag@gmail.com>
2020-10-03 21:58:06 +0200

    Added rotating portals prefab

---
+++ Assets/Scripts/RotatingPortals.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingPortals : MonoBehaviour
+{
+    void FixedUpdate ()
+    {
+        transform.Rotate (0,GameManager.Instance.Config.portalRotationSpeed*Time.deltaTime,0); //rotates 50 degrees per second around z axis
+    }
+}
