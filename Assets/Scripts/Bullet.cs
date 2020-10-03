8502a0d5609322e1d9ab4811ec25f716b6940d1b
daniel <danielholst2@gmail.com>
2020-10-03 17:09:50 +0200

    Add portals

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -6,6 +6,10 @@ public class Bullet : MonoBehaviour
 {
     private Vector3 m_direction;
 
+    private const float BULLET_SPEED = 8f;
+    private Vector3 offsetVecForward = new Vector3(0.0f, 0.0f, 0.5f);
+    private Vector3 offsetVecSide = new Vector3(0.5f, 0.0f, 0.0f);
+
     public void Fire(Vector3 direction)
     {
         m_direction = direction;
@@ -18,7 +22,31 @@ public class Bullet : MonoBehaviour
         Destroy(gameObject);
     }
 
-    private void Update()
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "portal1")
+        {
+            transform.position = GameObject.FindWithTag("portal2").transform.position + offsetVecForward;
+        }
+
+        if (collision.gameObject.tag == "portal2")
+        {
+            transform.position = GameObject.FindWithTag("portal1").transform.position - offsetVecForward;
+        }
+
+        if (collision.gameObject.tag == "portal3")
+        {
+            transform.position = GameObject.FindWithTag("portal4").transform.position + offsetVecSide;
+        }
+
+        if (collision.gameObject.tag == "portal4")
+        {
+            transform.position = GameObject.FindWithTag("portal3").transform.position - offsetVecSide;
+        }
+    }
+
+
+        private void Update()
     {
         transform.position += m_direction * Time.deltaTime * GameManager.Instance.Config.BulletSpeed;
     }
