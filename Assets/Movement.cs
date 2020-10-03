c9cf9530453c2f71a5a1876899152e6188742b57
daniel <danielholst2@gmail.com>
2020-10-03 12:52:37 +0200

    Add test scene with portals

---
+++ Assets/Movement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public Rigidbody body;
+    private Vector3 sideForceVec;
+    private Vector3 forwardForceVec;
+    private Vector3 upForceVec;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sideForceVec = new Vector3(10f, 0f, 0.0f);
+        forwardForceVec = new Vector3(0.0f, 0.0f, 10.0f);
+        upForceVec = new Vector3(0f, 5f, 0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("left"))
+        {
+            body.AddForce(-sideForceVec);
+        }
+
+        if (Input.GetKey("right")) {
+            body.AddForce(sideForceVec);
+        }
+
+        if (Input.GetKey("up"))
+        {
+            body.AddForce(forwardForceVec);
+        }
+
+        if (Input.GetKey("down"))
+        {
+            body.AddForce(-forwardForceVec);
+        }
+
+        if (Input.GetKey("space"))
+        {
+            body.AddForce(upForceVec);
+        }
+
+    }
+}
---
+++ Assets/PlayerPortal.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPortal : MonoBehaviour
+{
+    int limit = 9;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x > limit)
+            transform.position = new Vector3(-9, transform.position.y, transform.position.z);
+
+        if (transform.position.x < -limit)
+            transform.position = new Vector3(9, transform.position.y, transform.position.z);
+
+        if (transform.position.z < -limit)
+            transform.position = new Vector3(transform.position.x, transform.position.y, 9);
+
+        if (transform.position.z > limit)
+            transform.position = new Vector3(transform.position.x, transform.position.y, -9);
+    }
+}
