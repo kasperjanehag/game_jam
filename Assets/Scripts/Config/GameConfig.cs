2e6ecf8023736b6a4329904fbd082f1a823637e4
novalain <michael.f.noven@gmail.com>
2020-10-03 20:14:33 +0200

    Changes to player and input sensitivity

--- Assets/Scripts/Config/GameConfig.cs
+++ Assets/Scripts/Config/GameConfig.cs
@@ -6,11 +6,14 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "DefaultGameConfig", menuName = "ScriptableObjects/GameConfig", order = 1)]
 public class GameConfig : ScriptableObject
 {
+    [Header("Player")]
+    public float PlayerSpeed = 5f;
+    public float PlayerGravity = -10f;
+
     [Header("Shooting")]
     public float ShootDelay = 0.5f;
     public float BulletSpeed = 10f;
     public float BulletDeathTime = 2f;
-
     public float BulletDamage = 50f;
 
     [Header("Enemy")]
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private CharacterController m_characterController;
-    [SerializeField] private float m_speed;
     [SerializeField] private GameObject m_bullet;
+    [SerializeField] private float m_jumpHeight = 3f;
 
     private bool m_isShooting;
+
+    private Vector3 playerVelocity;
+    private bool isGrounded;
 
     void Start()
     {
@@ -20,9 +23,20 @@ public class PlayerController : MonoBehaviour
         var moveX = Input.GetAxis("Horizontal");
         var moveY = Input.GetAxis("Vertical");
 
+        isGrounded = m_characterController.isGrounded;
+        if (isGrounded && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+
         Vector3 move = new Vector3(moveX, 0, moveY);
-        var moveValue = move * Time.deltaTime * m_speed;
+        var moveValue = move * Time.deltaTime * GameManager.Instance.Config.PlayerSpeed;
         m_characterController.Move(moveValue);
+
+        if (move != Vector3.zero)
+        {
+            gameObject.transform.forward = move;
+        }
 
         Vector3 lookTarget = new Vector3();
         var ray = Camera.main.ScreenPointToRay (Input.mousePosition);
@@ -42,11 +56,14 @@ public class PlayerController : MonoBehaviour
             m_isShooting = true;
             StartCoroutine(SpawnBulletAfterDelay());
         }
+
+        playerVelocity.y += GameManager.Instance.Config.PlayerGravity * Time.deltaTime;
+        m_characterController.Move(playerVelocity * Time.deltaTime);
     }
 
     private IEnumerator SpawnBulletAfterDelay()
     {
-        var bullet = Instantiate(m_bullet, transform.position + transform.forward * 2f, Quaternion.identity);
+        var bullet = Instantiate(m_bullet, transform.position + transform.forward * 2f, transform.rotation);
         bullet.GetComponent<Bullet>().Fire(transform.forward);
         yield return new WaitForSeconds(GameManager.Instance.Config.ShootDelay);
         m_isShooting = false;
