c640efac601ff025989ea7f4ffc46f132122e395
novalain <michael.f.noven@gmail.com>
2020-10-03 16:05:38 +0200

    Add player prefab and bullet

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private Vector3 m_direction;
+
+    private const float BULLET_SPEED = 8f;
+
+    public void Fire(Vector3 direction)
+    {
+        m_direction = direction;
+        StartCoroutine(DestroyAfterDelay());
+    }
+
+    private IEnumerator DestroyAfterDelay()
+    {
+        yield return new WaitForSeconds(3f);
+        Destroy(gameObject);
+    }
+
+    private void Update()
+    {
+        transform.position += m_direction * Time.deltaTime * BULLET_SPEED;
+    }
+}
