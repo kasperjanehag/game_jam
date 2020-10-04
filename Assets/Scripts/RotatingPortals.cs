5e8381968fb4289ff4abcf91f6b3e1dc95f67b55
Kasper Janehag <kasper.janehag@gmail.com>
2020-10-04 17:43:26 +0200

    Rotating speed from config

--- Assets/Scripts/RotatingPortals.cs
+++ Assets/Scripts/RotatingPortals.cs
@@ -4,10 +4,8 @@ using UnityEngine;
 
 public class RotatingPortals : MonoBehaviour
 {
-    [SerializeField] private float m_rotationSpeed;
-
     void Update ()
     {
-        transform.Rotate (0, m_rotationSpeed*Time.deltaTime,0); //rotates 50 degrees per second around z axis
+        transform.Rotate (0, GameManager.Instance.Config.portalRotationSpeed*Time.deltaTime,0); //rotates 50 degrees per second around z axis
     }
 }
